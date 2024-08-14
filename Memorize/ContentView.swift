b6ee1606dfa5fc8c750ad060103673d133fbb1d0
AY <far.brest666@gmail.com>
2024-08-13 21:09:01 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Alks on 7/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .imageScale(.small)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alks on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
